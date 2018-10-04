229c440386090a0d6f4304f7e8463b5994250cd1
adrianno3259 <adriannosampaio@hotmail.com>
2018-10-04 01:03:09 -0300

    Create first test, and set test structure (pending cmake + Makefile)

--- tests/test_no_value_argument.cpp
+++ tests/test_no_value_argument.cpp
@@ -1,1 +1,26 @@
-#include "catch2/ca
+#define CATCH_CONFIG_MAIN
+#include "catch2/catch.cpp"
+#include "../SinpleArgParser.hpp"
+
+TEST_CASE()
+{
+    /// Test no value argument (--debug)
+    int argc = 2;
+
+    char *argv[] = {
+        "program",
+        "--debug"
+    };
+
+    parser::ArgumentParser arg(argc, argv);
+
+    arg.addArgument("--debug", false, "for debug messages");
+
+    arg.parse();
+
+    REQUIRE(arg.isDefined("--debug") == true);
+    REQUIRE(arg.isDefined("--debug") == false);
+
+}
+
+
